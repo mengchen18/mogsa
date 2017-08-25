fec2dd34a8cbce277d7e55bf23a250c9399b8ada
ga52cib <chen.meng@tum.de>
2017-08-25 15:33:05 +0200

    mbpca2 for row and column sparse

--- R/biSoftK.R
+++ R/biSoftK.R
@@ -1,5 +1,5 @@
 
-biSoftK <- function (x, maxiter, kp, kt) {
+biSoftK <- function (x, maxiter, kp, kt, unit.pb = TRUE, unit.tb = FALSE) {
   
   if (length(kp) < length(x)) 
     kp <- rep(kp, length.out = length(x))
@@ -8,19 +8,25 @@ biSoftK <- function (x, maxiter, kp, kt) {
   
   t <- svd(do.call("cbind", x))$u[, 1]
   
-  regproj <- function(xb, t, kp, kt) {
+  regproj <- function(xb, t, kp, kt, unit.pb, unit.tb) {
     pb <- t(xb) %*% t/c(t(t) %*% t)
+    if (!unit.pb)
+      pb <- normvec(pb)
     pb <- softK(pb, kp)
-    pb <- normvec(pb)
+    if (unit.pb)
+      pb <- normvec(pb)
     tb <- xb %*% pb
     tb <- softK(tb, kt)
-    tb <- normvec(tb)
+    if (unit.tb)
+      tb <- normvec(tb)
     list(tb = tb, pb = pb)
   }
   
   for (i in 1:maxiter) { 
     told <- t
-    rp <- mapply(SIMPLIFY = FALSE, function(x, y1, y2) regproj(x, t, y1, y2), x = x, y1 = kp, y2 = kt)
+    rp <- mapply(SIMPLIFY = FALSE, function(x, y1, y2) 
+      regproj(x, t, y1, y2, unit.pb = unit.pb, unit.tb = unit.tb), 
+      x = x, y1 = kp, y2 = kt)
     tm <- sapply(rp, "[[", "tb")
     w <- t(tm) %*% t/c(t(t) %*% t)
     w <- w/sqrt(sum(w^2))
