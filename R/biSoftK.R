eb241dff7ff0f0069657ffba85f90ee2ff6dce2b
ga52cib <chen.meng@tum.de>
2017-08-25 15:15:58 +0200

    add functions for concordance

---
+++ R/biSoftK.R
@@ -0,0 +1,36 @@
+
+biSoftK <- function (x, maxiter, kp, kt) {
+  
+  if (length(kp) < length(x)) 
+    kp <- rep(kp, length.out = length(x))
+  if (length(kt) < length(x))
+    kt <- rep(kt, length.out = length(x))
+  
+  t <- svd(do.call("cbind", x))$u[, 1]
+  
+  regproj <- function(xb, t, kp, kt) {
+    pb <- t(xb) %*% t/c(t(t) %*% t)
+    pb <- softK(pb, kp)
+    pb <- normvec(pb)
+    tb <- xb %*% pb
+    tb <- softK(tb, kt)
+    tb <- normvec(tb)
+    list(tb = tb, pb = pb)
+  }
+  
+  for (i in 1:maxiter) { 
+    told <- t
+    rp <- mapply(SIMPLIFY = FALSE, function(x, y1, y2) regproj(x, t, y1, y2), x = x, y1 = kp, y2 = kt)
+    tm <- sapply(rp, "[[", "tb")
+    w <- t(tm) %*% t/c(t(t) %*% t)
+    w <- w/sqrt(sum(w^2))
+    t <- tm %*% w
+    if (isTRUE(all.equal(c(t), c(told)))) 
+      break
+    if (i == maxiter) 
+      cat("  Note: maximum number of iterations was reached, algrithm may not converge.\n")
+  }
+  res <- list(tb = lapply(rp, "[[", "tb"), pb = lapply(rp, "[[", "pb"), t = t, w = w)
+  return(res)
+  
+}
