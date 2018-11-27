02e437e5d5290d2e75d920750c286570170cdaf2
mengchen18 <mengchen18@gmail.com>
2018-11-27 12:11:32 +0100

    projection of GS to a set of non-consecutive PCs are supported

--- R/sup.moa.R
+++ R/sup.moa.R
@@ -3,8 +3,20 @@
 #   sup - the supplementary table (nVar * nIndividual)
 #   axes - which axes should be used
 
-sup.moa <- function(X, sup, nf=2, 
+sup.moa <- function(X, sup, nf=2, factors = NULL, 
   ks.stat=FALSE, ks.B = 1000, ks.cores = NULL) {
+
+  if (is.null(nf) & is.null(factors))
+    stop("nf or factors need to be specified.")
+
+  if (!is.null(factors))
+    pcomp <- factors
+
+  if (!is.null(nf)) {
+    pcomp <- 1:nf
+    if (!is.null(factors))
+      warning("nf is given, factors will be ignored. If you want to specify factors, set nf=NULL.")
+  }
 
   # sup, nf
   N <- length(sup)
@@ -32,21 +44,21 @@ sup.moa <- function(X, sup, nf=2,
   normsup <- sup # change here
 
   GSCoordinate_sep <- mapply(SIMPLIFY=FALSE, function(load, sup, A) {
-    a <- t(sup * A) %*% as.matrix(load[, 1:nf, drop=FALSE])
-    colnames(a) <- paste("PC", 1:nf, sep="")
+    a <- t(sup * A) %*% as.matrix(load[, pcomp, drop=FALSE])
+    colnames(a) <- paste("PC", pcomp, sep="")
     return(a)
   }, load=load, sup=normsup, A = split(X@w.data, names(X@w.data))[nn])
 
   GSCoordinate_comb <- Reduce("+", GSCoordinate_sep)
   
   contribution <- lapply(GSCoordinate_sep, function(supcor, score) {
-    a <- lapply(1:nf, function(i) {
+    a <- lapply(pcomp, function(i) {
       r <- outer(supcor[, i], score[, i])
       colnames(r) <- rownames(score)
       return(r)
     })
     a[is.na(a)] <- 0
-    names(a) <- paste("PC", 1:nf, sep="")
+    names(a) <- paste("PC", pcomp, sep="")
     return(a)
   }, score=fs)
   
@@ -54,24 +66,24 @@ sup.moa <- function(X, sup, nf=2,
     Reduce("+", x)
   })
   
-  contribution_pc <- lapply(1:nf, function(i, cont) {
+  contribution_pc <- lapply(pcomp, function(i, cont) {
     a <- lapply(cont, function(x) x[[i]])
     Reduce("+", a)
   }, cont=contribution)
-  names(contribution_pc) <- paste("PC", 1:nf, sep="")
+  names(contribution_pc) <- paste("PC", pcomp, sep="")
   
   contribution_total <- Reduce("+", contribution_dataset) 
 
   csup <- do.call("rbind", sup)
   if (!ks.stat) {
     pmat <- .signifGS(X=X, sup=csup, A = X@w.data, 
-      score=contribution_total, nf=nf)
+      score=contribution_total, factors=pcomp)
     attr(pmat, "method") <- "zscore"
     } else {
       if (is.null(ks.cores)) 
         ks.cores <- getOption("mc.cores", 2L) 
       cat("running bootstrapping for p values of KS.stat ...\n")
-      pmat <- .ks.pval(X, sup, ks.B=ks.B, A = X@w.data, nf=nf, mc.cores = ks.cores)
+      pmat <- .ks.pval(X, sup, ks.B=ks.B, A = X@w.data, factors=pcomp, mc.cores = ks.cores)
       attr(pmat, "method") <- "KS.stat"
     }
   
@@ -89,7 +101,7 @@ sup.moa <- function(X, sup, nf=2,
 }
 
 
-.signifGS <- function(X, sup, A, score, nf) {
+.signifGS <- function(X, sup, A, score, factors) {
   
   # define function 
   ff <- function(x, n, score, infinite=FALSE) {
@@ -103,9 +115,9 @@ sup.moa <- function(X, sup, nf=2,
     2 * rowMin(cbind(pp, 1-pp))
   }
   # reconstuct matrix using nf PCs
-  U <- as.matrix(X@loading[, 1:nf, drop=FALSE]) 
-  D <- diag(sqrt(X@eig[1:nf]), nrow = nf)
-  V <- as.matrix(X@eig.vec[, 1:nf, drop=FALSE])
+  U <- as.matrix(X@loading[, factors, drop=FALSE]) 
+  D <- diag(sqrt(X@eig[factors]), nrow = length(factors))
+  V <- as.matrix(X@eig.vec[, factors, drop=FALSE])
   rec <- (U %*% D %*% t(V)) * A
   # the number of feature in each GS
   supn <- colSums(sup != 0)
